371fc8f7ef9034c702c5f745adcc7cc684a978c5
Le Hoang <icy.lord.love.angel@gmail.com>
2016-10-13 19:02:28 +0700

    Fix edit

--- customize_target_files.sh
+++ customize_target_files.sh
@@ -6,7 +6,7 @@ TARGET_FILES_DIR=$OUT_DIR/target_files
 build_prop_file=$TARGET_FILES_DIR/SYSTEM/build.prop
 OTHER_DIR=$PWD/other
 
-if [ -e $METADATA_DIR/updater ]
+if [ -e $OTHER_DIR/updater ]
 then
    echo "Use custom updater bin file"
    cp $OTHER_DIR/updater $TARGET_FILES_DIR/OTA/bin
